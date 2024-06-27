8ed02315281d6b6713bb71ff67000a454a6911de
zhorzhevnaElena <zhorzhevna.elena@yandex.ru>
2024-06-28 02:43:01 +0300

    Реализовано ТЗ 10 спринта.

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,18 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmorateApplication {
-    private final static Logger log = LoggerFactory.getLogger(FilmorateApplication.class);
-
     public static void main(String[] args) {
-        log.info("Before Starting application");
         SpringApplication.run(FilmorateApplication.class, args);
-        log.debug("Starting my application in debug with {} args", args.length);
-        log.info("Starting my application with {} args.", args.length);
     }
 }
